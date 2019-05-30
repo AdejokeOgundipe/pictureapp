597828c429dafa414f06352257574d7145362be4
Adejoke <Adejoke@DESKTOP-TN8OFN2>
2019-05-30 08:48:16 +0100

    initial2

--- Homepage.py
+++ Homepage.py
@@ -1,12 +1,16 @@
-from flask import Flask,render_template,request,url_for,redirect,session
+from flask import Flask,render_template,request,url_for,redirect,session, jsonify,flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,BooleanField,form
 from wtforms.validators import InputRequired, Email, Length
 import pyodbc
 import os
+from flask_restful import Resource, Api
+#from flask_dropzone import Dropzone
+import base64
 
-#password=sha256_crypt.encrypt(str(form.password.data))
+APP_ROOT =os.path.dirname(os.path.abspath(__file__))
+
 # database connection using pyodbc 
 conn = pyodbc.connect(
     "Driver={SQL Server Native Client 11.0};"
@@ -15,12 +19,13 @@ conn = pyodbc.connect(
     "Trusted_Connection=yes; ")
 cursor=conn.cursor()
 # session assignment
-
+#session['username'] = 'username'
 app= Flask(__name__)
+
 # secret key assignment
 app.config['SECRET_KEY'] = 'mysecretthings'
-
 Bootstrap(app)
+api=Api(app)
 
 # class definition for action Login
 class loginform(FlaskForm):
@@ -35,27 +40,28 @@ class Registrationform(FlaskForm):
     username=StringField('Username',validators=[InputRequired(),Length(min=4, max=15)])
     password = PasswordField('Password', validators=[InputRequired(),Length(min=8, max=80)])
     
-# session['username'] = 'username'
+ 
 # definition of each route in the website
 @app.route('/')
 def MyWebsite():
-    return redirect(url_for('Myhome'))
+     return redirect(url_for('Myhome'))
+   
 
 
 @app.route('/home')
 def Myhome():
     if 'username' in session:
-        username=session['username']
-        return 'Logged in as ' + username
+        # username=session['username']
+        # return 'Logged in as ' + username
+        return render_template('home.html')
     return redirect(url_for('login'))
-    #"You have not logged in <br /> <a href='/Login'><br />" +  "click here to log in <br /> </a>"
-    # return render_template('home.html')
+   
 
 @app.route('/Login', methods=['GET','POST'])
 def login():  
     form = loginform(request.form) 
     # session.clear()
-        
+   
     if form.validate_on_submit():
      # obtaining data from database
         cursor.execute('SELECT [Username],[Password] FROM [User] WHERE Username=? AND Password= ?',(form.username.data, form.password.data))
@@ -66,10 +72,11 @@ def login():
             return redirect(url_for('dashboard'))
         
 
-        return "Invalid Username and password <br />"+ " <a href='/Registration'><br />" +  "click here to Register <br /> </a>"
-    
+        return "Invalid Username and password <br />"+ "Not yet registered?" + "<a href='/Registration'><br />" +  "click here to Register <br /> </a>"
+       
 
     return render_template('login.html',form=form)
+
 
     
 # route showing post and get method for registration page
@@ -79,26 +86,46 @@ def Registration():
     if form.validate_on_submit():
         cursor.execute('insert into [User](Name,Email,Password,Username) values (?,?,?,?);',(form.name.data, form.Email.data, form.password.data, form.username.data))
         cursor.commit()
-
-        return "Your Registration was successful <br /><a href='/Login'><br />" +  "click here to log in <br /> </a>"
+        return "Your Registration was successful <br />" + "<a href='/Login'><br />" +  "click here to log in <br /> </a>"
   
     return render_template('Registration.html',form=form)
 
 
-@app.route('/dashboard')
+@app.route('/dashboard',methods=["GET","POST"])
 def dashboard():
     if 'username' in session:
+       
+        cursor.commit()
         return render_template('dashboard.html')
-    else:
-        return redirect(url_for("login"))
+
+
+         
+@app.route('/dashjson', methods=["POST"])
+def dashjson():
+    data = request.get_json()
+    name=data['name']
+    username=data['username']
+    password=data['password']
+    Email=data['Email']
+    return jsonify({'result' : 'success!', 'name' : name, 'username' : username, 'password' : password, 'Email': Email})
+
+      
+
 
 @app.route('/About')
 def About():
-    return render_template('About.html')
+    if 'username' in session:
+        return render_template('About.html')
+    else:
+        return redirect(url_for("login"))
+   
 
 @app.route('/Contact')
 def Contact():
-    return render_template('Contact.html')
+    if 'username' in session:
+        return render_template('Contact.html')
+    else:
+        return redirect(url_for("login"))
 
 @app.route('/logout')
 def Logout():
@@ -112,6 +139,55 @@ def profile():
 @app.route('/Home')
 def Home():
     return render_template('home.html')
+
+@app.route('/upload',methods=["GET"])
+def upload():
+    #file=request.files['inputfile']
+
+    return render_template('upload.html') 
+
+@app.route('/upload',methods=["POST"])
+def upload_post():
+    target = os.path.join(APP_ROOT, 'static\\')
+    print(target)
+    try:
+
+        if  os.path.isdir(target):
+            os.mkdir(target)
+        # else:
+        #     print("couldn't create upload directory: {}".format(target))
+    except:
+        print('just continue please')
+    file = request.files['inputFile']
+
+    print("{} is the file name".format(file.filename))
+    str = base64.b64encode(file.read())
+    #print(str)
+    cursor.execute('''INSERT INTO Picture(
+                            
+                            UserId,
+                            Name,
+                            ActualString,
+                            Caption) 
+                            VALUES(?,?,?,?)''',(1,"image","logic",str))
+    cursor.commit()
+    print('saved successfully')
+
+    # for converting the string back to image in the sql server
+
+    cursor.execute('SELECT * FROM Picture')
+    myresults= cursor.fetchall()
+    print(myresults)
+    # converting str back to image
+    # img=base64.b85decode(str)
+    # return (file.filename)
+    return redirect(url_for('dashboard'))
+
+# @app.route('/upload/<filename>')
+# def send_image('filename'):
+#     return send_from directory('static',filename)
+
+
 
 
 
--- app.py
+++ app.py
@@ -13,10 +13,11 @@
 # @app.route('/layout')
 # def layout():
 #     return render_template("layout.html")
-
+# .L{
+#       background-color: blue
+#     }
 # @app.route('/welcome')
 # def welcome():
-#     return render_template('welcome.html')
 
 # @app.route('/registration',methods=["POST"])
 # def Register():
@@ -92,4 +93,77 @@
 # if __name__ == '__main__':
 #     app.run(debug=True)
 
+# class PhotoForm(FlaskForm):
+#     photo = FileField(validators=[FileRequired()])
 
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     if form.validate_on_submit():
+#         f = form.photo.data
+#         filename = secure_filename(f.filename)
+#         f.save(os.path.join(
+#             app.instance_path, 'photos', filename
+#         ))
+#         return redirect(url_for('index'))
+
+#     return render_template('upload.html', form=form)
+#  <h1> Dashboard</h1>
+
+# <body>
+#         <form action ="/" method="POST">
+#             <h1> The breaking through</h1>
+#         </form>
+#     </form>
+#     </body> 
+#password=sha256_crypt.encrypt(str(form.password.data))
+# inserting data into the database 
+ #cursor.execute(INSERT INTO ['User'].Picture values (1, (SELECT * FROM OPENROWSET(BULK N'static:\Pt.jpg', SINGLE_BLOB) as Image1)))
+
+
+
+
+#dashboard
+  #if 'username' in session:
+        # cursor.execute('SELECT [Caption],[UserId] FROM Picture')
+        # cursor.commit()
+        # data = request.get_json()
+        # username=data['username']
+        # password=data['password']
+        # return jsonify({'result' : 'success!',  'username' : username, 'password' : password})
+        # result= list(cursor)
+        # return jsonify({"key": result})
+    #return render_template('dashboard.html') 
+    # 
+    #  
+    # @app.route('api/Users', methods=['GET','POST'])
+# def api_User():
+#     if request.method='POST':
+#         result=request.get_json()
+#     else:
+#         all_Users={}
+#         users=Users.query.all()
+ 
+ 
+ # return jsonify('Username')
+        #api.add_resource('Hello')
+
+         #"You have not logged in <br /> <a href='/Login'><br />" +  "click here to log in <br /> </a>"
+    # return render_template('home.html')
+     # return jsonify({'student': "ade"})
+
+
+     #upload
+      # destination = "".join([target, filename])
+    # print ("Accept incoming file", filename)
+    # print ("save it to :", destination)
+    
+    
+    
+    # conversion of image to string
+    # with open(filePath,"rb") as image:
+     # with open("Pk.jpg", "rb") as image:
+    #     f = image.read()
+    #     b = bytearray(f)
+    #     print (b[0])
+
+    #file.save(destination)
