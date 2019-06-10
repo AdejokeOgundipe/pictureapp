69db46c243c1106cdf5ea69e52b77ee1f3128bbf
Adejoke <Adejoke@DESKTOP-TN8OFN2>
2019-06-10 12:14:41 +0100

    db commit

--- Homepage.py
+++ Homepage.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,url_for,redirect,session, jsonify,flash,send_from_directory,make_response
+from flask import Flask,render_template,request,url_for,redirect,session, jsonify,flash,send_from_directory,make_response,json
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,BooleanField,form
@@ -6,7 +6,6 @@ from wtforms.validators import InputRequired, Email, Length
 import pyodbc
 import os
 from flask_restful import Resource, Api
-#from flask_dropzone import Dropzone
 import base64
 
 APP_ROOT =os.path.dirname(os.path.abspath(__file__))
@@ -47,17 +46,6 @@ def MyWebsite():
      return redirect(url_for('Myhome'))
    
 
-
-# @app.route('/home')
-# def Myhome():
-#     if 'username' in session:
-#         # username=session['username']
-#         # return 'Logged in as ' + username
-       
-#         return render_template('home.html')
-#     return redirect(url_for('login'))
-   
-
 @app.route('/Login', methods=['GET','POST'])
 def login():  
     form = loginform(request.form) 
@@ -70,7 +58,7 @@ def login():
         if (len(list(cursor))> 0):
             session['username'] = request.form['username']
         
-            return redirect(url_for('dashboard'))
+            return redirect(url_for('Myhome'))
         
 
         return "Invalid Username and password <br />"+ "Not yet registered?" + "<a href='/Registration'><br />" +  "click here to Register <br /> </a>"
@@ -79,7 +67,6 @@ def login():
     return render_template('login.html',form=form)
 
 
-    
 # route showing post and get method for registration page
 @app.route('/Registration', methods=['GET','POST'])
 def Registration():
@@ -87,93 +74,23 @@ def Registration():
     if form.validate_on_submit():
         cursor.execute('insert into [User](Name,Email,Password,Username) values (?,?,?,?);',(form.name.data, form.Email.data, form.password.data, form.username.data))
         cursor.commit()
-        return "Your Registration was successful <br />" + "<a href='/Login'><br />" +  "click here to log in <br /> </a>"
+        # return "Your Registration was successful <br />" + "<a href='/Login'><br />" +  "click here to log in <br /> </a>"
+        return redirect(url_for('login'))
   
     return render_template('Registration.html',form=form)
+
 
 # dashboard
 @app.route('/dashboard',methods=["GET","POST"])
 def dashboard():
     if 'username' in session:
-        
-    #    cursor.execute('SELECT [Caption] FROM Picture')
-    #    results= cursor.fetchall()
-    # #    response = make_response(base64.b64decode(results))
-    # #result = base64.b64decode(results)
-    # # image_string = list(results)[0][0]
-    # #print(results)
-
-    # images = []
-
-    # for row in results:
-    #     if (row[0] is None):
-    #         images.append(None)
-    #     else:
-    #         images.append(base64.b64encode(row[0]))
-
-    
-      
-    # return render_template('dashboard.html',images = images)
-
-    #image_names=os.listdir('./static')
-    
         return render_template('dashboard.html')
-
-
-         
-
-
-
-# @app.route('/upload',methods=["POST"])
-# def upload_post():
-#     target = os.path.join(APP_ROOT, 'static\\')
-#     print(target)
-#     try:
-
-#         if  os.path.isdir(target):
-#             os.mkdir(target)
-#         # else:
-#         #     print("couldn't create upload directory: {}".format(target))
-#     except:
-#         print('just continue please')
-#     file = request.files['inputFile']
-
-#     print("{} is the file name".format(file.filename))
-#     str = base64.b64encode(file.read())
-#     #print(str)
-#     cursor.execute('''INSERT INTO Picture(
-                            
-#                             UserId,
-#                             Name,
-#                             ActualString,
-#                             Caption) 
-#                             VALUES(?,?,?,?)''',(1,"image","logic",str))
-#     cursor.commit()
-#     print('saved successfully')
-
-#     # for converting the string back to image in the sql server
-
-#     # cursor.execute('SELECT * FROM Picture')
-#     # myresults= cursor.fetchall()
-#     # print(myresults)
-#     # # converting str back to image
-#     # img=base64.b85decode(str)
-#     # return (file.filename)
-#     return redirect(url_for('dashboard'))
-    # return send_from_directory("static",filename , as_attachment=True)
-
-# @app.route('/upload/<filename>')
-# def send_image('filename'):
-#     return send_from directory('static',filename)
 
 
 @app.route('/upload',methods=["GET"])
 def upload():
     #file=request.files['inputfile']
-
     return render_template('upload.html') 
-
-
 
 
 @app.route('/upload',methods=["POST"])
@@ -191,28 +108,22 @@ def upload_post():
         filename= file.filename
         destination = "".join([target, filename])
         print("Accept incoming file", filename)
-        print("save it to :", destination)
+        print("save it to :", 
+        )
         file.save(destination)
-    #return render_template("dashboard.html")
-    #return send_from_directory("static", filename, as_attachment=True)
-
-
-    #path=base64.b85decode(filename)
-    path = base64.b64encode(file.read())
-    cursor.execute('''INSERT INTO Picture(
+        print("filename is : " , filename)
+        cursor.execute('''INSERT INTO Picture(
                             
                             UserId,
                              Name,
                              ActualString,
-
-
-                              Caption) 
-                            VALUES(?,?,?,?)''',(3,"Adejoke","images",path))
-    cursor.commit()
-    return render_template('dashboard.html', image_name= filename)
-
-
-
+                             Caption) 
+                            VALUES(?,?,?,?)''',(1,"Adejoke","images",filename))
+        cursor.commit()
+        return render_template('dashboard.html', image_name= filename)
+    
+    
+    
 @app.route('/upload/<filename>')
 def send_image(filename):
     return send_from_directory('static',filename)
@@ -222,58 +133,33 @@ def send_image(filename):
 def Myhome():
     if 'username' in session:
         image_names=os.listdir('./static')
-        
-    
         return render_template('home.html',image_names=image_names)
     return redirect(url_for('login'))
     
-@app.route('/device')
-def dbCon():
-    if 'username' in session:
-        
-        path = base64.b64encode(os.listdir('./static/'))
-       
-        cursor.execute('INSERT INTO  Picture(UserId,Name,ActualString,Caption) VALUES(?,?,?,?)',(3,"Remmy","img",path))
-        cursor.commit()
-        return "successful"
-    return ('dashbord.html')
-    
-    
-    
-    #return render_template('home.html',image_names=image_names)
-     # image_name=os.listdir('./static/')
-        # path = base64.b64encode(image_name)
 
 @app.route('/test')
 def binaryToImage():
     cursor.execute('SELECT [Caption] FROM Picture ')
     my_results= cursor.fetchall()
-
     image_string = list(my_results)[0][4]
-    # for pic in my_results:
-    #     print(list(pic))
-    # file =  open(os.path.join(APP_ROOT, 'static\\') + "testImage.jpg", "wb")
-    # file.write(base64.b64decode(image_string))
-    # file.close()
-
     response = make_response(base64.b64decode(image_string))
     response.headers.set('Content-Type', 'image/jpeg')
     response.headers.set('Content-Disposition', 'attachment', filename = "test.jpg")
-
     return response
     
 
-@app.route('/dashjson', methods=["POST"])
-def dashjson():
-    data = request.get_json()
-    name=data['name']
-    username=data['username']
-    password=data['password']
-    Email=data['Email']
-    return jsonify({'result' : 'success!', 'name' : name, 'username' : username, 'password' : password, 'Email': Email})
+@app.route('/return_json', methods=["GET","POST"])
+def return_json():
+    cursor.execute('SELECT [Caption]  FROM Picture ')
+    data = list(cursor.fetchall())
+    result = []
 
-      
-
+    for t in data:
+        result.append(os.path.join(APP_ROOT, 'static\\') + t[0])
+    
+    return jsonify(result)
+  
+   
 
 @app.route('/About')
 def About():
@@ -298,9 +184,6 @@ def Logout():
 @app.route('/Profile')
 def profile():
     return render_template('Profile.html')
-
-
-
 
 
 if __name__ == '__main__':
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # import os
 # from flask import Flask,render_template,request
+#from flask_dropzone import Dropzone
 # app = Flask(__name__)
 
 # @app.route('/')
@@ -120,7 +121,45 @@
  #cursor.execute(INSERT INTO ['User'].Picture values (1, (SELECT * FROM OPENROWSET(BULK N'static:\Pt.jpg', SINGLE_BLOB) as Image1)))
 
 
+# @app.route('/test')
+# def binaryToImage():
+#     cursor.execute('SELECT [Caption] FROM Picture ')
+#     my_results= cursor.fetchall()
 
+#     image_string = list(my_results)[0][4]
+    # for pic in my_results:
+    #     print(list(pic))
+    # file =  open(os.path.join(APP_ROOT, 'static\\') + "testImage.jpg", "wb")
+    # file.write(base64.b64decode(image_string))
+    # file.close()
+
+    # response = make_response(base64.b64decode(image_string))
+    # response.headers.set('Content-Type', 'image/jpeg')
+    # response.headers.set('Content-Disposition', 'attachment', filename = "test.jpg")
+
+    # return response
+    
+#     @app.route('/dashjson', methods=["GET","POST"])
+# def dashjson():
+    # data = request.get_json()
+    # name=data['name']
+    # username=data['username']
+    # password=data['password']
+    # Email=data['Email']
+    #return jsonify({'result' : 'success!', 'name' : name, 'username' : username, 'password' : password, 'Email': Email})
+    
+    # cursor.execute('SELECT [Caption]  FROM Picture ')
+    # data = cursor.fetchall()
+    # print(data)
+    #result=len(list[data])
+   
+    #image_string = list(data)[1][2]
+    # return jsonify({'result :',data})
+    # #return jsonify(list(data))
+    #return render_template('dashboard.html')
+    # print(my_results)
+    # return jsonify(data)
+      
 
 #dashboard
   #if 'username' in session:
@@ -211,7 +250,14 @@
 # def send_image('filename'):
 #     return send_from directory('static',filename)
 
-
+# @app.route('/home')
+# def Myhome():
+#     if 'username' in session:
+#         # username=session['username']
+#         # return 'Logged in as ' + username
+       
+#         return render_template('home.html')
+#     return redirect(url_for('login'))
 
 # for pic in my_results:
     #     print(list(pic))
@@ -274,3 +320,107 @@
 
 #cursor.execute('INSERT INTO  Picture(UserId,Name,ActualString,Caption) VALUES(?,?,?,?)',(1,"Remmy","img",image_names))
     # cursor.commit()
+
+
+    # dashboard
+# @app.route('/dashboard',methods=["GET","POST"])
+# def dashboard():
+#     if 'username' in session:
+        
+    #    cursor.execute('SELECT [Caption] FROM Picture')
+    #    results= cursor.fetchall()
+    # #    response = make_response(base64.b64decode(results))
+    # #result = base64.b64decode(results)
+    # # image_string = list(results)[0][0]
+    # #print(results)
+
+    # images = []
+
+    # for row in results:
+    #     if (row[0] is None):
+    #         images.append(None)
+    #     else:
+    #         images.append(base64.b64encode(row[0]))
+
+    
+      
+    # return render_template('dashboard.html',images = images)
+
+    #image_names=os.listdir('./static')
+    
+        #return render_template('dashboard.html')
+
+
+        #upload
+        # @app.route('/upload',methods=["POST"])
+# def upload_post():
+#     target = os.path.join(APP_ROOT, 'static\\')
+#     print(target)
+#     try:
+
+#         if  os.path.isdir(target):
+#             os.mkdir(target)
+#         # else:
+#         #     print("couldn't create upload directory: {}".format(target))
+#     except:
+#         print('just continue please')
+#     file = request.files['inputFile']
+
+#     print("{} is the file name".format(file.filename))
+#     str = base64.b64encode(file.read())
+#     #print(str)
+#     cursor.execute('''INSERT INTO Picture(
+                            
+#                             UserId,
+#                             Name,
+#                             ActualString,
+#                             Caption) 
+#                             VALUES(?,?,?,?)''',(1,"image","logic",str))
+#     cursor.commit()
+#     print('saved successfully')
+
+#     # for converting the string back to image in the sql server
+
+#     # cursor.execute('SELECT * FROM Picture')
+#     # myresults= cursor.fetchall()
+#     # print(myresults)
+#     # # converting str back to image
+#     # img=base64.b85decode(str)
+#     # return (file.filename)
+#     return redirect(url_for('dashboard'))
+    # return send_from_directory("static",filename , as_attachment=True)
+
+# @app.route('/upload/<filename>')
+# def send_image('filename'):
+#     return send_from directory('static',filename)
+
+#filename
+#path=base64.b85decode(filename)
+     #image_names=os.listdir('./static')
+    #newfile= (name=file.filename, data=file.read())
+    # images=file.save(os.path.join(('./static'), filename))
+
+
+    # @app.route('/device')
+# def dbCon():
+#     if 'username' in session:
+        
+#         path = base64.b64encode(os.listdir('./static/'))
+       
+#         cursor.execute('INSERT INTO  Picture(UserId,Name,ActualString,Caption) VALUES(?,?,?,?)',(3,"Remmy","img",path))
+#         cursor.commit()
+#         return "successful"
+#     return ('dashbord.html')
+    
+    
+    
+    #return render_template('home.html',image_names=image_names)
+     # image_name=os.listdir('./static/')
+        # path = base64.b64encode(image_name)
+
+
+        #print(myresult)
+    #print(myresult[0])
+    # data= request.get( cursor.execute('SELECT [Caption]  FROM Picture '))
+    #print(data)
+    # print(json.dumps(data))
